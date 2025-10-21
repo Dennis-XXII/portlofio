b98f16d419992946222ce15b1c317d9754f74b56
Kyaw Swar Hein <kyawswarhein555225@gmail.com>
2025-10-21 14:15:11 +0700

    More data and profile photo fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,9 +9,9 @@ export default function Experience({ setActive }) {
   useEffect(() => { if (inView) setActive("experience"); }, [inView, setActive]);
 
   return (
-    <section id="experience" ref={ref} className="section">
+    <section id="experience" className="section">
       <div className="container">
-        <h2 className="h2">Experiences</h2>
+        <h2 className="h2"  ref={ref} >Experiences</h2>
 
         <div className="stack-gap">
           {experiences.map((exp, i) => (
@@ -20,10 +20,10 @@ export default function Experience({ setActive }) {
                 <img src={exp.image} alt={exp.imgAlt} className="square" />
                 <div>
                 <div className="kv" style={{borderBottom:"none"}}>
-                  <h3 className="h3">{exp.title} </h3>
+                  <h3 className="h3">{exp.role} </h3>
                   <div style={{textAlign:"left", color:"var(--ink-2)"}}>{exp.years}</div>
                 </div>
-                <span style={{fontWeight:400}}>({exp.role})</span>
+                <span style={{fontWeight:400, color:"var(--ink-2)"}}>({exp.title})</span>
                   <p style={{marginTop:10}}>{exp.description}</p>
                 </div>
               </div>
